671e52a7ecf9d05593d43426c8b24b519be26e44
Hasan Veldstra <hasan.veldstra@gmail.com>
2009-10-24 21:45:33 +0100

    Make key optional in read()

--- vertexdb.py
+++ vertexdb.py
@@ -2,6 +2,17 @@ import urllib2
 from urllib2 import urlopen
 import simplejson as json
 
+def split_path(path):
+    comps = [c for c in path.split("/") if c != ""]
+    if len(comps) == 1:
+        parent = "/"
+        last = comps[0]
+    else:
+        parent = "/" + "/".join(comps[0:-1]) + "/"
+        last = path.split("/")[-1]
+
+    return [parent, last]
+
 class VertexDb:
     
     def __init__(self, host="http://localhost:8080"):
@@ -13,8 +24,13 @@ class VertexDb:
     def write(self, path, key, val):
         return urlopen("%s%s%s%s" % (self.host, path, "?action=write&key=", key), val).read()
         
-    def read(self, path, key):
-        return urlopen("%s%s%s%s" % (self.host, path, "?action=read&key=", key)).read()
+    def read(self, path, key=None):
+        # If key is not given, use the last component of path as key.
+        if key is not None:
+            return urlopen("%s%s%s%s" % (self.host, path, "?action=read&key=", key)).read()
+        else:
+            [parent, key] = split_path(path)
+            return self.read(parent, key)
         
     def mkdir(self, path):
       return urlopen("%s%s%s" % (self.host, path, "?action=mkdir")).read()
@@ -23,31 +39,25 @@ class VertexDb:
         return urlopen("%s%s%s" % (self.host, path, "?action=size")).read()
     
     def pairs(self, path):
-        return urlopen("%s%s%s" % (self.host, path, "?action=select&op=pairs")).read()
+        return json.loads(urlopen("%s%s%s" % (self.host, path, "?action=select&op=pairs")).read())
     
     def keys(self, path):
-        return urlopen("%s%s%s" % (self.host, path, "?action=select&op=keys")).read()
+        return json.loads(urlopen("%s%s%s" % (self.host, path, "?action=select&op=keys")).read())
     
     def stat(self, path):
         print "Not supported in vertexdb yet."
         return urlopen("%s%s%s" % (self.host, path, "?action=stat")).read()
-        
+    
     def is_dir(self, path):
-        if path == "/":
-            return True
-            
         # FIXME: Returns False on empty directories. (Need stat in vertexdb.)
         # FIXME: is_dir("/test/k/") returns None when /test/k is a file.
+
+        if path == "/":
+            return True
         
-        comps = [c for c in path.split("/") if c != ""]
-        if len(comps) == 1:
-            parent = "/"
-            last = comps[0]
-        else:
-            parent = "/" + "/".join(comps[0:-1]) + "/"
-            last = path.split("/")[-1]
+        [parent, last] = split_path(path)
         
-        pairs_json = json.loads(self.pairs(parent))
+        pairs_json = self.pairs(parent)
         
         for e in pairs_json:
             if e[0] == last:
@@ -64,6 +74,7 @@ print "read: ", vdb.read("/test/", "akey")
 print "keys: ", vdb.keys("/")
 print "pairs: ", vdb.pairs("/")
 print "is_dir: ", vdb.is_dir("/test/")
+print "read: ", vdb.read("/test/akey")
 
 # print vdb.stat("/test/")
 
